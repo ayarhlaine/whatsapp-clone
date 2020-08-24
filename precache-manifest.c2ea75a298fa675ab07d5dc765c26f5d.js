5efe39923648c1f15221cfa8c897fb503f70fdbd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-24 18:02:20 +0000

    Deploy ayarhlaine/whatsapp-clone to github.com/ayarhlaine/whatsapp-clone.git:gh-pages

---
+++ precache-manifest.c2ea75a298fa675ab07d5dc765c26f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377016bbf8e9b3ca84f8f294943bc004",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "6be3362aeb5b3b87f722",
+    "url": "/whatsapp-clone/static/css/main.509f7612.chunk.css"
+  },
+  {
+    "revision": "8c956064ccd8120cc1f0",
+    "url": "/whatsapp-clone/static/js/2.11c164c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/whatsapp-clone/static/js/2.11c164c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be3362aeb5b3b87f722",
+    "url": "/whatsapp-clone/static/js/main.953bc4fe.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
